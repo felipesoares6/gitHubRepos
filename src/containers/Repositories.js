9014e695a26dd6ce7cdef71d4ed13326af2653b6
felipe.soares <felipeluizsoares@gmail.com>
2017-12-19 21:42:54 -0200

    Update Repositories component to show a list with repos data from GitHub Api

--- src/containers/Repositories.js
+++ src/containers/Repositories.js
@@ -1,13 +1,55 @@
 import React from 'react'
-import { View, Text, TextInput, Button } from 'react-native'
-import { withState, pure, compose } from 'recompose'
+import { FlatList } from 'react-native'
+import { pure, compose } from 'recompose'
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
 
-const Repositories = ({ username }) => {
-  return (
-    <View>
-      <Text>{ username }</Text>
-    </View>
-  )
-}
+import displayLoading from '../components/Loading'
+import displayError from '../components/Error'
+
+import Repository from '../components/Repository'
+
+const keyExtractor = (item) => item.id
+
+const renderItems = ({ item }) => (
+  <Repository
+    key={ `${item.id}` }
+    repository={ item }
+  />
+)
+
+const RepositoriesPure = ({ data }) => (
+  <FlatList
+    data={data.user.repositories.nodes}
+    keyExtractor={keyExtractor}
+    renderItem={renderItems}
+  />
+)
+
+const data = graphql(gql`
+  query repositoriesSearch($username: String!){
+    user(login: $username) {
+      repositories(last: 10) {
+        nodes {
+          id,
+          nameWithOwner,
+          description
+        }
+      }
+    }
+  }
+`, {
+  options: ({ username }) => ({
+    variables: { username: username, type: 'String' }
+  })
+})
+
+
+const Repositories = compose(
+  data,
+  displayLoading,
+  displayError,
+  pure
+)(RepositoriesPure)
 
 export default Repositories
