91df6cb838a93f7dbc3fa5d515fc2914803158bc
felipe.soares <felipeluizsoares@gmail.com>
2017-12-18 22:05:25 -0200

    Pass a theme by ThemeProvider to the App

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Text, View } from 'react-native'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo'
 import { setContext } from 'apollo-link-context'
 import { createHttpLink } from 'apollo-link-http'
-import { Text, View } from 'react-native'
+import { ThemeProvider } from 'styled-components/native';
 
+import theme from './src/theme'
 import Root from './src/components/Root'
 
 const TOKEN = '2cd92775eb3e72a098665227aba01dedfd882409'
@@ -30,7 +32,9 @@ const client = new ApolloClient({
 const App = () => {
   return (
     <ApolloProvider client={ client }>
-      <Root />
+      <ThemeProvider theme={ theme }>
+        <Root />
+      </ThemeProvider>
     </ApolloProvider>
   )
 }
