8dd31a23dacfc3689265b6ab8500d3edc5d7d1f6
felipe.soares <felipeluizsoares@gmail.com>
2017-12-20 23:10:25 -0200

    Update Error feedback

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { pure, compose, branch, renderComponent } from 'recompose'
+import styled from 'styled-components/native'
+
+const ErrorTitle = styled.Text`
+  font-size: 18px;
+  color: ${ props => props.theme.secondaryDarkColor };
+  text-align: center;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 90%;
+`
+
+const ErrorText = ErrorTitle.extend`
+  font-size: 16px;
+  color: ${ props => props.theme.colorText };
+`
 
 const Error = () => (
   <View>
-    <Text> Oops! Something went wrong :( </Text>
-    <Text> Try again and remember to search for a valid GitHub username! </Text>
+    <ErrorTitle> Oops! Something went wrong :( </ErrorTitle>
+    <ErrorText> Try again and remember to search for a valid GitHub username! </ErrorText>
   </View>
 )
 
